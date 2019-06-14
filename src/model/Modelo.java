15f44c8e04fa5ea0b0b5d12339aea0aff30a76a1
José Evamberto <evamberto.lima@gmail.com>
2019-06-13 21:16:29 -0300

    inicial

---
+++ src/model/Modelo.java
@@ -0,0 +1,23 @@
+package model;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author jose
+ */
+public class Modelo {
+    
+    public Modelo(){
+        
+    
+    }
+    
+    
+    
+    
+}
