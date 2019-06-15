6e6cb5105385f254d5a1da50d6c758d2f81d0aa4
José Evamberto <evamberto.lima@gmail.com>
2019-06-14 23:18:02 -0300

    Feito gerarFNC

--- src/model/Modelo.java
+++ src/model/Modelo.java
@@ -80,12 +80,12 @@ public class Modelo {
     
     
     public void gerarFNC(){
-        int cor=1;
+        int cor=4;
         if (cor==1) {
             String fnc = "";
         for (int i = 1; i <= vertices; i++) {
-             fnc=""+i+cor+"\n";
-             System.out.print(fnc);//Escrever no arquivo
+             fnc=""+i+cor+" 0\n";
+            /* System.out.print(fnc);//Escrever no arquivo*/
             
         }
          
@@ -93,14 +93,73 @@ public class Modelo {
             for (int i = 0; i < this.matriz.length; i++) {
                 for (int j = 0; j < this.matriz.length; j++) {
                     if (this.matriz[i][j]==1) {
-                        fnc=""+(i*-1)+" "+(j*-1)+"\n";
-                        System.out.print(fnc);
+                        fnc=""+(i*-1)+"1"+" "+(j*-1)+"1"+" 0\n";
+                     /*   System.out.print(fnc);//escrever no arquivo*/
                         
                         /*vou ṕegar um vertice colorir 11 e comparar com seus vizinhos -21*/
                     }
                 }
             }
          
+            
+        }else if(cor>1){
+            String fnc="";
+            
+            for (int i = 1; i <=this.vertices; i++) {
+                for (int j = 1; j <= cor; j++) {
+                    
+                    fnc=fnc+i+j+" 0";
+                   
+                    
+                }
+                 /*System.out.println(fnc); Escrever no arquivo*/
+                fnc="";
+            }
+            fnc="";
+            String auxFnc;
+            for (int i = 1; i <=this.vertices; i++) {
+                
+                
+                for (int j = 1; j <=cor; j++) {
+                      fnc="-"+i+j;
+                      for (int k = 1; k <=cor; k++) {
+                          if (j!=k) {
+                              fnc=fnc+" "+"-"+i+k+" 0";
+                              /*System.out.println(fnc);Escrever no arquivo*/
+                          }
+                          fnc="";
+                          fnc="-"+i+j;
+                          
+                    }
+                }
+                
+            }
+            
+            String fnc1="";
+  
+            for (int i = 0; i < this.matriz.length; i++) {
+                for (int j = 0; j < this.matriz.length; j++) {
+                    if (this.matriz[i][j]==1) {
+                      
+                        for (int k = 1; k <= cor; k++) {
+                             fnc1="-"+i+k;
+                             for (int l =1; l <= cor; l++) {                                 
+                                 if (k==l) {
+                                    fnc1=fnc1+" "+"-"+j+l+" 0";
+                                     System.out.println(fnc1);
+                                 }
+                                
+                            }
+                             fnc1="";
+                        }
+                     /*   System.out.print(fnc);//escrever no arquivo*/
+                        
+                        /*vou ṕegar um vertice colorir 11 e comparar com seus vizinhos -21*/
+                    }
+                }
+            }
+            
+            
             
         }
        
