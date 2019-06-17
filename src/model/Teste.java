52911b68d5459f9b38d8f0bac8ce36ca209288f2
José Evamberto <evamberto.lima@gmail.com>
2019-06-17 15:25:48 -0300

    First Version

--- src/model/Teste.java
+++ src/model/Teste.java
@@ -7,6 +7,8 @@ package model;
 
 import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.logging.Level;
@@ -23,7 +25,7 @@ public class Teste {
         Modelo m = new Modelo();
         File file = new File("Instancias");
         File arquivos[] = file.listFiles();
-        System.out.println("Escolha o graph:");
+        System.out.println("Escolha o grafo:");
         for (int i = 0; i < arquivos.length; i++) {
             System.out.println(i + "-" + arquivos[i].getName());
 
@@ -41,8 +43,8 @@ public class Teste {
         if (es >= 0 && es < arquivos.length) {
             m.GerarMatrizAdjacencia(arquivos[es]);
             long tempoInicio = System.currentTimeMillis();
-            for (int i = 1; i <= m.getMaxCor(); i++) {
-
+           for (int i = 1; i <=m.getMaxCor(); i++) {
+            
                 m.gerarFNC(arquivos[es], i);
 
                 try {
@@ -50,13 +52,14 @@ public class Teste {
                     BufferedReader in = new BufferedReader(new InputStreamReader(processo.getInputStream()));
 
                     String line;
+                    
                     while ((line = in.readLine()) != null) {
                         if (line.equals("s SATISFIABLE")) {
                             System.out.println("É satisfazível");
                             System.out.println(line);
                             isSatisfativel=true;
                         } else {
-                            //System.out.println("Não é satisfazível");
+                        
                            // System.out.println(line);
                             isSatisfativel=false;
                         }
@@ -70,16 +73,20 @@ public class Teste {
                 
                 if(isSatisfativel){
                     menorCor=i;
-                    break;
+                   break;
                 }
+                
 
             }
             long tempoFim=System.currentTimeMillis()-tempoInicio;
             
             double segundos=(double)tempoFim;
+            String arquivo=arquivos[es].getName()+".cnf";
+           
+            imprimirArquivo(arquivo);
             
             System.out.println("Quantidade minima de cores:"+menorCor);
-          //  System.out.println("Tempo:"+segundos);
+          
              System.out.printf("%.3f ms%n", (tempoFim) / 1000d);
 
 
@@ -87,6 +94,28 @@ public class Teste {
 
         }
 
+    }
+    
+    public static void imprimirArquivo(String nomeArquivo){
+        File file = new File(nomeArquivo);
+        try {
+            FileReader ler = new FileReader(file);
+            
+            BufferedReader lerArq = new BufferedReader(ler);
+            
+            
+              String s;
+            String[] split;
+
+            while ((s = lerArq.readLine()) != null) {
+            
+                System.out.println(s);
+            }
+        } catch (FileNotFoundException ex) {
+            Logger.getLogger(Teste.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (IOException ex) {
+            Logger.getLogger(Teste.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
 
 }
